61504d27f5a4d4864ef369a910bcc214bad31bcd
Dhian <muthidp@gmail.com>
2024-11-25 10:46:11 +1100

    Fix build

--- apps/migration/src/app/app.module.ts
+++ apps/migration/src/app/app.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
